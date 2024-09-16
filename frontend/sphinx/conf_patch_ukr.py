2035b9d39c7c5b8d41c39a416b9a2a8012c1b857
Maxim Reznik <reznikmm@gmail.com>
2024-09-17 00:19:55 +0300

    Enable Ablog extension for blog posts.

--- frontend/sphinx/conf_patch_ukr.py
+++ frontend/sphinx/conf_patch_ukr.py
@@ -7,11 +7,28 @@ exclude_patterns += [
 ]
 
 extensions += [
+    'ablog',
     "myst_parser",
 ]
+
+
+blog_authors = {
+        'max': ("Максим Резник", "https://github.com/reznikmm")
+}
+
+blog_default_language = 'uk'
+
+blog_languages = {
+    'uk': ('Україньска', None),
+}
+
+blog_post_pattern = ["posts/*.md"]
 
 html_logo = "img/logo_ukr.svg"
 
 language = 'uk'
+
+myst_update_mathjax = False # ablog docs sets this
+
 
 # master_doc = 'index_ukr'
