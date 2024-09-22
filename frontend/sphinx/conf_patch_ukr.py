3ad771bd590d6df9df30a3b329ed6815cd9fd9b2
Maxim Reznik <reznikmm@gmail.com>
2024-09-22 13:38:20 +0300

    Switch html_theme to `pydata_sphinx_theme`
    because it supports Ablog extension. Add `posts.rst` and `cources.md` to
    have corresponding top level sections. The `pydata_sphinx_theme` draws
    top level section at the top of the page and removes them from the side bar.
    So we add `navbar-nav` to `index` side-bar for better visibility.

--- frontend/sphinx/conf_patch_ukr.py
+++ frontend/sphinx/conf_patch_ukr.py
@@ -27,14 +27,28 @@ copyright = u'2023 – 2024, Ada Ukraine. All rights reserved. CC BY 4.0 License
 
 html_logo = "img/logo_ukr.svg"
 
+html_sidebars = {
+   'index': [
+          'navbar-nav',
+          'ablog/postcard.html', 'ablog/recentposts.html',
+          'ablog/tagcloud.html', 'ablog/categories.html',
+          'ablog/archives.html',
+         ],
+   '**': [
+          'sidebar-nav-bs',
+          'ablog/postcard.html', 'ablog/recentposts.html',
+          'ablog/tagcloud.html', 'ablog/categories.html',
+          'ablog/archives.html',
+         ]
+}
+
+html_theme = "pydata_sphinx_theme"
+
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
 }
 
 language = 'uk'
 
 myst_update_mathjax = False # ablog docs sets this
-
 
 # master_doc = 'index_ukr'
